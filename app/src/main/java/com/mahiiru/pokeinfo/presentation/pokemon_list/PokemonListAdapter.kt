ebb2053cbfcad7265bd32b7d87483c921d4cfdc7
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-06 18:34:53 -0300

    PokemonListAdapter.kt and PokemonListViewHolder.kt implemented.

--- app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListAdapter.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListAdapter.kt
@@ -1,4 +1,21 @@
 package com.mahiiru.pokeinfo.presentation.pokemon_list
 
-class PokemonListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mahiiru.pokeinfo.R
+import com.mahiiru.pokeinfo.domain.model.Pokemon
+
+class PokemonListAdapter(
+    private val pokemons : List<Pokemon>)
+    :RecyclerView.Adapter<PokemonListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonListViewHolder = PokemonListViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_pokemon_list,parent,false)
+    )
+
+    override fun getItemCount(): Int = pokemons.size
+
+    override fun onBindViewHolder(holder: PokemonListViewHolder, position: Int) {
+        holder.bind(pokemons[position])
+    }
 }
--- app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListViewHolder.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListViewHolder.kt
@@ -1,4 +1,19 @@
 package com.mahiiru.pokeinfo.presentation.pokemon_list
 
-class PokemonListViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.mahiiru.pokeinfo.databinding.ItemPokemonListBinding
+import com.mahiiru.pokeinfo.domain.model.Pokemon
+import com.squareup.picasso.Picasso
+
+class PokemonListViewHolder(itemView : View)
+    : RecyclerView.ViewHolder(itemView) {
+
+        private val binding = ItemPokemonListBinding.bind(itemView)
+
+    fun bind(pokemon : Pokemon){
+        Picasso.get().load(pokemon.sprite).into(binding.ivPokemonList)
+        binding.tvPokemonListID.text = pokemon.id.toString()
+        binding.tvPokemonListName.text = pokemon.name.replaceFirstChar { it.uppercaseChar() }
+    }
 }
