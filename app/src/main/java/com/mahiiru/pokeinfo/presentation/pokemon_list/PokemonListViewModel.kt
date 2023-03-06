df16e7d95bd6e15e1659ded95a2076b7ac64155d
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-06 16:01:33 -0300

    PokemonListViewModel.kt implemented.

--- app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListViewModel.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_list/PokemonListViewModel.kt
@@ -1,8 +1,14 @@
 package com.mahiiru.pokeinfo.presentation.pokemon_list
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mahiiru.pokeinfo.common.Resource
 import com.mahiiru.pokeinfo.domain.use_case.GetPokemonsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 
@@ -13,5 +19,30 @@ class PokemonListViewModel @Inject constructor(
 
 ) : ViewModel() {
 
+
+    private val _state = mutableStateOf(PokemonListState())
+    val state : State<PokemonListState> = _state
+
+    init {
+        getPokemons()
+    }
+
+    private fun getPokemons() {
+
+        getPokemonsUseCase().onEach { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _state.value = PokemonListState(pokemons = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = PokemonListState(error = "An unexpected error occurred.")
+                }
+                is Resource.Loading -> {
+                    _state.value = PokemonListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+
+    }
 
 }
