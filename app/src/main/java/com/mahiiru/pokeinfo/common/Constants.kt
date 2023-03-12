6011c9c3f4a2e48483abf1e224bc4149c7f71251
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-12 14:16:07 -0300

    EXTRA_ID Constants.kt created.

--- app/src/main/java/com/mahiiru/pokeinfo/common/Constants.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/common/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "https://pokeapi.co/api/v2/"
     const val SPRITE_START_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
     const val SPRITE_END_URL = ".png"
+    const val EXTRA_ID = "extra_id"
 }
