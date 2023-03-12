120a261d76e82d9cd64dd89597d97246469ca9d8
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-12 14:14:11 -0300

    PokemonDetailsActivity.kt created.

---
+++ app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_details/PokemonDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.mahiiru.pokeinfo.presentation.pokemon_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahiiru.pokeinfo.R
+
+class PokemonDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_details)
+    }
+}
