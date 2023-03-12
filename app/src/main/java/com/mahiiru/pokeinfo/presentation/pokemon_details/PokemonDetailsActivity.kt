81a8f4003325f01d6d504956c6508dc3f9b325fe
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-12 14:22:02 -0300

    PokemonDetailsActivity.kt implemented.

--- app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_details/PokemonDetailsActivity.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/presentation/pokemon_details/PokemonDetailsActivity.kt
@@ -3,10 +3,23 @@ package com.mahiiru.pokeinfo.presentation.pokemon_details
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mahiiru.pokeinfo.R
+import com.mahiiru.pokeinfo.common.Constants.EXTRA_ID
+import com.mahiiru.pokeinfo.databinding.ActivityPokemonDetailsBinding
 
 class PokemonDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPokemonDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokemon_details)
+        binding = ActivityPokemonDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
+
+        getPokemonDetails(id)
+    }
+
+    private fun getPokemonDetails(id: String) {
+        TODO("Not yet implemented")
     }
 }
