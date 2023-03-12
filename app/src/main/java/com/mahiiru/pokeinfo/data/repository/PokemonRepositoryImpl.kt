86727d8ac188281c98f6c35cb465f721722f70d8
Cristóbal Pavez <c.pavez07@ufromail.cl>
2023-03-12 16:03:38 -0300

    getPokemonDetails implemented.

--- app/src/main/java/com/mahiiru/pokeinfo/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/data/repository/PokemonRepositoryImpl.kt
@@ -1,8 +1,11 @@
 package com.mahiiru.pokeinfo.data.repository
 
 import com.mahiiru.pokeinfo.data.remote.PokeApi
+import com.mahiiru.pokeinfo.data.remote.dto.PokemonDetailsDTO
 import com.mahiiru.pokeinfo.data.remote.dto.toPokemon
+import com.mahiiru.pokeinfo.data.remote.dto.toPokemonDetails
 import com.mahiiru.pokeinfo.domain.model.Pokemon
+import com.mahiiru.pokeinfo.domain.model.PokemonDetails
 import com.mahiiru.pokeinfo.domain.repository.PokemonRepository
 import javax.inject.Inject
 
@@ -12,4 +15,6 @@ class PokemonRepositoryImpl @Inject constructor(
 
 
     override suspend fun getPokemons(): List<Pokemon> = api.getPokemonsDTO().results.map { it.toPokemon() }
+
+    override suspend fun getPokemonDetails(pokemonId : Int): PokemonDetails = api.getPokemonDetailsDTOById(pokemonId).toPokemonDetails()
 }
--- app/src/main/java/com/mahiiru/pokeinfo/domain/repository/PokemonRepository.kt
+++ app/src/main/java/com/mahiiru/pokeinfo/domain/repository/PokemonRepository.kt
@@ -1,8 +1,11 @@
 package com.mahiiru.pokeinfo.domain.repository
 
 import com.mahiiru.pokeinfo.domain.model.Pokemon
+import com.mahiiru.pokeinfo.domain.model.PokemonDetails
 
 interface PokemonRepository {
 
     suspend fun getPokemons() : List<Pokemon>
+
+    suspend fun getPokemonDetails(pokemonId : Int) : PokemonDetails
 }
